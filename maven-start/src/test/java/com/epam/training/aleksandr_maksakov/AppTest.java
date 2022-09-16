efef5d8b96dabb13456c45fb9a709fefb809027b
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2022-09-16 19:16:03 +0300

    The 1-st course "Maven Build Tool" is finished with the practical task "I Can Win" in the folder "maven-start"

---
+++ maven-start/src/test/java/com/epam/training/aleksandr_maksakov/AppTest.java
@@ -0,0 +1,20 @@
+package com.epam.training.aleksandr_maksakov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
