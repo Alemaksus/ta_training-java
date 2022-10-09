ebec52fe727eac87fea50a7912aa74ce7aa6c9ca
Alemaksus <alexander.maksakov@gmai.com>
2022-10-09 23:38:07 +0300

    files for Practices: First Programs, Data Type, Сonditions and Сycles

--- Java_Basic/com/epam/training/aleksandr_maksakov/FirstProgram.java
+++ Java_Basic/com/epam/training/aleksandr_maksakov/FirstProgram.java
@@ -2,6 +2,18 @@ package com.epam.training.aleksandr_maksakov;
 
 public class FirstProgram {
     public static void main(String[] args) {
-
+//        int i = 3;
+//        byte b = 1;
+//        byte b1 = 1 + 2;                // line 1
+//        short s = 304111;               // line 2
+//        short s1 = (short) 304111;       // line 3
+//        b = b1 + 1;                     // line 4
+//        b = (byte)  (b1 + 1);           // line 5
+//        b = −b;                         // line 6
+//        b = (byte) −b;                  // line 7
+//        b1 *= 2;                       // line 8
+//        b = i;                          // line 9
     }
 }
+
+
