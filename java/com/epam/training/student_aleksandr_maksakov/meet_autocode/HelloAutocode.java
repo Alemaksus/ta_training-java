537156f1d6d3a9dd3a697365ab3154bb81155673
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2022-11-01 22:18:52 +0400

    Done

---
+++ java/com/epam/training/student_aleksandr_maksakov/meet_autocode/HelloAutocode.java
@@ -0,0 +1,8 @@
+package com.epam.training.student_aleksandr_maksakov.meet_autocode;
+
+public class HelloAutocode {
+    public static void main(String[] args) {
+        //Write a program, printing "Hello, Autocode!"
+       System.out.println("Hello, Autocode!");
+    }
+}
