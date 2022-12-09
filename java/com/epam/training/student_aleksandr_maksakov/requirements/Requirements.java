0cf41d461603c03b16e65610fb049bd5960c20a6
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2022-12-09 10:52:20 +0400

    2-nd release

---
+++ java/com/epam/training/student_aleksandr_maksakov/requirements/Requirements.java
@@ -0,0 +1,46 @@
+package com.epam.training.student_aleksandr_maksakov.requirements;
+
+public class Requirements {
+
+    public static void requireNonNull(Object obj) {
+        if (obj == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    public static void requireNonNull(Object obj, String message) {
+        if (obj == null) {
+            throw new NullPointerException(message);
+        }
+    }
+
+    public static void checkArgument(boolean value) {
+        if (!value) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkArgument(boolean value, String message) {
+        if (!value) {
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public static void checkState(boolean value) {
+        if (!value) {
+            throw new IllegalStateException();
+        }
+    }
+
+    public static void checkState(boolean value, String message) {
+        if (!value) {
+            throw new IllegalStateException(message);
+        }
+    }
+
+    public static void checkIndex(int index, int size) {
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+}
