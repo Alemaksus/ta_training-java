c07fe3245bb04f65d07ea2584aaca3a17f9fb3a1
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2023-02-23 17:23:42 +0400

    new dev

--- java/com/epam/training/student_aleksandr_maksakov/framework/src/test/java/driver/DriverSingleton.java
+++ java/com/epam/training/student_aleksandr_maksakov/framework/src/test/java/driver/DriverSingleton.java
@@ -9,23 +9,18 @@ import java.time.Duration;
 public class DriverSingleton {
 
     private static WebDriver driver;
-    public static long PAGE_LOAD_TIMEOUT = 20;
-    public static long IMPLICIT_WAIT = 10;
+
 
     private DriverSingleton(){}
 
     public static WebDriver getDriver(){
         if (null == driver){
-            switch (System.getProperty("browser")){
-                case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                }
-                default: {
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                }
+            if ("firefox".equals(System.getProperty("browser"))) {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
             }
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
             driver.manage().window().maximize();
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
