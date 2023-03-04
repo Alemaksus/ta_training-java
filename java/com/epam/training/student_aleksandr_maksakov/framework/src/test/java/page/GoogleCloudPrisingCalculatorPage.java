a2ab4346eb9b4cbf98de37f5e1a5ef0e026d37d0
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2023-03-04 17:48:22 +0400

    executor.executeScript("window.scrollTo(0,200)", "");

--- java/com/epam/training/student_aleksandr_maksakov/framework/src/test/java/page/GoogleCloudPrisingCalculatorPage.java
+++ java/com/epam/training/student_aleksandr_maksakov/framework/src/test/java/page/GoogleCloudPrisingCalculatorPage.java
@@ -230,30 +230,29 @@ public class GoogleCloudPrisingCalculatorPage extends AbstractPage {
     public GoogleCloudPrisingCalculatorPage clickSendEmailButton() throws InterruptedException {
 
         JavascriptExecutor executor = (JavascriptExecutor) driver;
-        executor.executeScript("window.scrollTo(0,500)", ""); //- it works
+        executor.executeScript("window.scrollTo(0,200)", ""); //- it works
 
         new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
-                .until(ExpectedConditions.elementToBeClickable(sendEmailButton)).click();
+                .until(ExpectedConditions.elementToBeClickable(sendEmailButton)).click();//- it works
         return this;
     }
 
-//        WebElement myElement = new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOf(button));
-//    ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", myElement);
-//a.moveToElement(webElement).build().perform();
-
+//        JavascriptExecutor executor = ((JavascriptExecutor) driver);
+//        executor.executeScript("arguments[0].scrollIntoView();", sendEmailButton);
+//
+//        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
+//                .until(ExpectedConditions.elementToBeClickable(sendEmailButton)).click();
+//        return this;
+//    }
 
 //        Actions actions = new Actions(driver);
 //        Thread.sleep(2000);
 //        actions.moveToElement(sendEmailButton).perform(); - it does not work: with the error:
 //        org.openqa.selenium.interactions.MoveTargetOutOfBoundsException: move target out of bounds
-
 //        WebElement element = driver.findElement(sendEmailButton);
 //        new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
 //                .until(ExpectedConditions.elementToBeClickable(element));
 //        executor.executeScript("arguments[0].scrollIntoView();", element);
-
-
-
 
     public GoogleCloudPrisingCalculatorPage inputCopiedEmail() {
         new WebDriverWait(driver, Duration.ofSeconds(WAIT_TIMEOUT_SECONDS))
