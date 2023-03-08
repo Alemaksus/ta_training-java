ea2d05bc84021976e89e999806b7fda847e3a857
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2023-03-08 18:39:57 +0400

    driver.manage().window().setSize(new Dimension(1028, 639));

--- java/com/epam/training/student_aleksandr_maksakov/framework/src/test/java/driver/DriverSingleton.java
+++ java/com/epam/training/student_aleksandr_maksakov/framework/src/test/java/driver/DriverSingleton.java
@@ -29,7 +29,7 @@ public class DriverSingleton {
                     driver = new ChromeDriver();
                 }
             }
-            driver.manage().window().setSize(new Dimension(1920, 1080));
+            driver.manage().window().setSize(new Dimension(1028, 639));
             driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
